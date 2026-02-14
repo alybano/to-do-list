4b1ad19357f66a99b92b392a357fcd0720ec4710
alybano <alyssabano24@gmail.com>
2026-02-14 22:19:57 +0800

    Changes you made

--- client/src/components/api.js
+++ client/src/components/api.js
@@ -4,6 +4,9 @@ import axios from "axios";
 // Base URL from Vite env
 const API_URL = import.meta.env.VITE_API_URL;
 
+// Debug log to confirm what value is being used
+console.log("API URL:", API_URL);
+
 // Create axios instance
 export const axiosInstance = axios.create({
   baseURL: API_URL,
